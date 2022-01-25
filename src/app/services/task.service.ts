8d9b9d8b697642748c32c2f279038bca90d339bd
nhussaini <nasrullah_hussaini@yahoo.com>
2022-01-25 17:04:54 -0500

    fetch data from the local server

--- src/app/services/task.service.ts
+++ src/app/services/task.service.ts
@@ -8,9 +8,11 @@ import { TASKS } from '../mock-tasks';
   providedIn: 'root',
 })
 export class TaskService {
-  constructor() {}
+  private apiUrl = 'http://localhost:5000/tasks';
+  constructor(private http: HttpClient) {}
   getTasks(): Observable<Task[]> {
-    const tasks = of(TASKS);
-    return tasks;
+    // const tasks = of(TASKS);
+    //return tasks;
+    return this.http.get<Task[]>(this.apiUrl);
   }
 }
